4d50909fcdcea6ca713f589cb5fc15c96e413349
durru <danielurrutxua@opendeusto.es>
2024-07-03 19:51:41 +0200

    fix metric and dimension filters

--- src/modules/utils/resources.py
+++ src/modules/utils/resources.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-initial_phrases_df = pd.read_csv("../resources/sinonimos_dime.csv")
-metrics_df = pd.read_csv("../resources/metrics_reduced.csv")
-dimensions_df = pd.read_csv("../resources/dimensions_reduced.csv")
-english_words_df = pd.read_csv("../resources/4000-most-common-english-words-csv.csv")
+initial_phrases_df = pd.read_csv("resources/sinonimos_dime.csv")
+metrics_df = pd.read_csv("resources/metrics_reduced.csv")
+dimensions_df = pd.read_csv("resources/dimensions_reduced.csv")
+english_words_df = pd.read_csv("resources/4000-most-common-english-words-csv.csv")
 
 
 
@@ -36,41 +36,31 @@ def export_csv(pairs: list):
         metrics = ";".join(api_query.get('metrics', []))
         dimensions = ";".join(api_query.get('dimensions', []))
         
-        #date_ranges = api_query.get('dateRanges', [{}])[0]
-        #start_date = date_ranges.get('startDate', "")
-        #end_date = date_ranges.get('endDate', "")
+        date_ranges = api_query.get('dateRanges', [{}])[0]
+        start_date = date_ranges.get('startDate', "")
+        end_date = date_ranges.get('endDate', "")
         
         dimension_filter = ""
         if 'dimensionFilter' in api_query:
             filter_expression = api_query['dimensionFilter'].get('filter', {})
             field_name = filter_expression.get('fieldName', "")
-            if 'stringFilter' in filter_expression:
-                match_type = filter_expression['stringFilter'].get('matchType', "")
-                value = filter_expression['stringFilter'].get('value', "")
-                dimension_filter = f"{field_name};{match_type};{value}"
-            elif 'numericFilter' in filter_expression:
-                operation = filter_expression['numericFilter'].get('operation', "")
-                value = filter_expression['numericFilter'].get('value', {}).get('int64Value', "")
-                dimension_filter = f"{field_name};{operation};{value}"
+            match_type = filter_expression['fieldFilter'].get('match', "")
+            value = filter_expression['fieldFilter'].get('value', "")
+            dimension_filter = f"{field_name};{match_type};{value}"
 
         metric_filter = ""
         if 'metricFilter' in api_query:
             filter_expression = api_query['metricFilter'].get('filter', {})
             field_name = filter_expression.get('fieldName', "")
-            if 'stringFilter' in filter_expression:
-                match_type = filter_expression['stringFilter'].get('matchType', "")
-                value = filter_expression['stringFilter'].get('value', "")
-                metric_filter = f"{field_name};{match_type};{value}"
-            elif 'numericFilter' in filter_expression:
-                operation = filter_expression['numericFilter'].get('operation', "")
-                value = filter_expression['numericFilter'].get('value', {}).get('int64Value', "")
-                metric_filter = f"{field_name};{operation};{value}"
+            match_type = filter_expression['fieldFilter'].get('match', "")
+            value = filter_expression['fieldFilter'].get('value', "")
+            dimension_filter = f"{field_name};{match_type};{value}"
         
-        #rows.append([natural_language_query, metrics, dimensions, start_date, end_date, dimension_filter, metric_filter])
-        rows.append([natural_language_query, metrics, dimensions, dimension_filter, metric_filter])
+        rows.append([natural_language_query, metrics, dimensions, start_date, end_date, dimension_filter, metric_filter])
+        #rows.append([natural_language_query, metrics, dimensions, dimension_filter, metric_filter])
 
-    #df = pd.DataFrame(rows, columns=['natural_language_query', 'metrics', 'dimensions', 'start_date', 'end_date', 'dimensionFilter', 'metricFilter'])
-    df = pd.DataFrame(rows, columns=['natural_language_query', 'metrics', 'dimensions', 'dimensionFilter', 'metricFilter'])
+    df = pd.DataFrame(rows, columns=['natural_language_query', 'metrics', 'dimensions', 'start_date', 'end_date', 'dimensionFilter', 'metricFilter'])
+    #df = pd.DataFrame(rows, columns=['natural_language_query', 'metrics', 'dimensions', 'dimensionFilter', 'metricFilter'])
 
     df.to_csv('output.csv', index=False, encoding='utf-8')
     print("Exportación a CSV completada.")
